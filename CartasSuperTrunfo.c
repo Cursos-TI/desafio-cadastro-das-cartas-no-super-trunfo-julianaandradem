a5c3ce635266c1f77745c23a0cfe123c7af47101
julianaandradem <julianaandrademattos@gmail.com>
2025-02-20 12:48:00 +0000

    Commit edit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,29 +1,47 @@
 #include <stdio.h>
 
-int main(){
-printf("desafio super trunfo! \n");
-printf("Novo commit\n")
-
-
-}
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+#include <stdio.h>
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    int codigo;
+    char nome[50];
+    int populacao;
+    float area;
+    double pib;
+    int pontos_turisticos;
+
+    // Mensagem inicial
+    printf("Desafio Super Trunfo - Cadastro de Cartas\n\n");
+
+    // Captura de dados do usuário
+    printf("Digite o código da cidade: 021");
+    scanf("%d", &codigo);
+
+    printf("Digite o nome da cidade: Rio de Janeiro ");
+    scanf("%\n", nome);  
+
+    printf("Digite a população da cidade: 6.000.000");
+    scanf("%d", &populacao);
+
+    printf("Digite a área da cidade em km²: 1.200");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da cidade em bilhões: 331,28");
+    scanf("%lf", &pib);
+
+    printf("Digite o número de pontos turísticos da cidade: 58");
+    scanf("%d", &pontos_turisticos);
+
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("\n=== Dados da Cidade Cadastrada ===\n");
+    printf("Código: %d\n", codigo);
+    printf("Nome: %s\n", nome);
+    printf("População: %d habitantes\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2lf bilhões\n", pib);
+    printf("Pontos Turísticos: %d\n", pontos_turisticos);
 
     return 0;
 }
+
